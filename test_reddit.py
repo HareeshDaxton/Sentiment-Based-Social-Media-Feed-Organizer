1233321ac765ca6c26aee83b8d52320933ba1ba0
Hareesh Kumar <hareeshdaxton@gmail.com>
2025-10-08 08:07:17 +0530

    Update test_reddit.py

--- test_reddit.py
+++ test_reddit.py
@@ -1,7 +1,7 @@
 import praw
 
 reddit = praw.Reddit(
-    client_id="AEJVauFgASesRFuip_1VoQ",
+    client_id="YOUR CLIENT ID",
     client_secret="eC26Ck0q9FyuAbE_yS1JYGQH9NrEjQ",
     user_agent="reddit_pipeline_app"
 )
